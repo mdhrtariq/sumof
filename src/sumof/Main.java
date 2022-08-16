8fe3c167682a9b4ac1eecdf72542c3fa8a142edd
mdhrtariq <thariq22@gmail.com>
2022-08-16 19:24:44 +0530

    sumof

---
+++ src/sumof/Main.java
@@ -0,0 +1,23 @@
+package sumof;
+public class Main {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!" + "   " +"sum of two number");
+        int m=4;
+        int n=7;
+        int t1=m+n;
+        int t2=m-n;
+        int t3=m*n;
+        int t4=m/n;
+        int t5=m%n;
+        {
+            System.out.println(t1);
+            System.out.println(t2);
+            System.out.println(t3);
+            System.out.println(t4);
+            System.out.println(t5);
+        }
+
+    }
+}
